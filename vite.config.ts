64aa433f2c38c563557514d14e9bc4ddcd7b6b8d
a111182119-prog <a111182119@nkust.edu.tw>
2026-05-26 19:47:00 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/A111182119/' ,
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
